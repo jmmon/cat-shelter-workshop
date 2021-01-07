11d2a708d5b542f3d02b1279e25bb42586c1e359
jmmon <jmmon93@gmail.com>
2021-01-06 21:46:56 -0600

    working server

---
+++ handlers/static-files.js
@@ -0,0 +1,46 @@
+function getContentType(url) {
+    if (url.endsWith('css')){
+        return 'text/css';
+    } else if(url.endsWith('http')) {
+        return 'text/http';
+    } else if(url.endsWith('png')) {
+        return 'png';
+    } else if(url.endsWith('js')) {
+        return 'js';
+    }
+}
+
+module.exports = (req,res) => {
+    const pathname = url.parse(req.url).pathname;
+
+    if (pathname.startsWith('/content') && req.method === 'GET') {
+        //TODO...
+        //read a file via readFile
+        //check for errors
+        //deliver correct content type
+        //send correct response with data received from the fs module
+
+        fs.readFile(`./${filepath}`, 'utf-8', (err,data) => {
+            if (err) {
+                console.log(err);
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                });
+
+                res.write("Error was found");
+                res.end();
+                return;
+            }
+            console.log(pathname);
+            res.writeHead(
+                200,
+                { 'Content-Type': getContentType(pathname) }
+            );
+
+            res.write(data);
+            res.end();
+        })
+    } else {
+        return true;
+    }
+}
